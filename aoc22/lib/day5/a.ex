4cc7e8ab6106ebf62eb14b964e7a578ba4cf1e93
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2022-12-04 08:52:46 +0100

    Kickstart day 5

---
+++ aoc22/lib/day5/a.ex
@@ -0,0 +1,7 @@
+defmodule AoC22.Day5.A do
+  # alias AoC22.Utils
+
+  def solve(_input) do
+    :result
+  end
+end
---
+++ aoc22/test/day5/a_test.exs
@@ -0,0 +1,9 @@
+defmodule AoC22.Day5.ATest do
+  use ExUnit.Case
+
+  alias AoC22.Day5.A
+
+  test "solve" do
+    assert A.solve("input/5_test_1.txt") == :result
+  end
+end
