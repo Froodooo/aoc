de53738f6359cb2d8f0dd44f2d1d653a722b065a
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2022-12-08 08:18:56 +0100

    Progress for day 8 part 2

--- aoc22/lib/day8/b.ex
+++ aoc22/lib/day8/b.ex
@@ -6,6 +6,7 @@ defmodule AoC22.Day8.B do
     |> Utils.input()
     |> to_2d_map()
     |> count_visible_trees()
+    |> Enum.max()
   end
 
   defp count_visible_trees(trees) do
@@ -18,54 +19,81 @@ defmodule AoC22.Day8.B do
 
   defp visible({x, y}, trees) do
     IO.inspect({{y, x}, trees[y][x]})
+
     visible_from_top({x, y}, trees) *
     visible_from_bottom({x, y}, trees) *
     visible_from_left({x, y}, trees) *
     visible_from_right({x, y}, trees)
-    |> IO.inspect()
   end
 
-  defp visible_from_top({x, y}, trees) do
-    Enum.reduce_while((y - 1)..0, 0, fn next_y, count ->
-      if trees[next_y][x] < trees[next_y + 1][x] do
+  defp count_next_trees([next_tree], tree, count) do
+    if next_tree < tree, do: {:cont, count + 1}, else: {:halt, count + 1}
+  end
+
+  defp count_next_trees([next_tree1, next_tree2], tree, count) do
+    if next_tree1 <= next_tree2 do
+      if next_tree1 <= tree do
         {:cont, count + 1}
       else
-        {:halt, (if count == 0, do: 1, else: count)}
+        {:halt, count + 1}
       end
-    end)
+    else
+      {:halt, count + 1}
+    end
+  end
+
+  defp chunk_trees([tree_pair]), do: [[tree_pair]]
+  defp chunk_trees(tree_pair), do: Enum.chunk_every(tree_pair, 2, 1)
+
+  defp visible_from_top({x, y}, trees) do
+    tree = trees[y][x]
+
+    (y - 1)..0
+      |> Enum.reduce([], &([trees[&1][x] | &2]))
+      |> Enum.reverse()
+      |> chunk_trees()
+      |> Enum.reduce_while(0, fn tree_pair, count ->
+        count_next_trees(tree_pair, tree, count)
+      end)
     |> IO.inspect(label: :top)
   end
 
   defp visible_from_bottom({x, y}, trees) do
-    Enum.reduce_while((y + 1)..(Enum.count(trees) - 1), 0, fn next_y, count ->
-      if trees[next_y][x] < trees[next_y - 1][x] do
-        {:cont, count + 1}
-      else
-        {:halt, (if count == 0, do: 1, else: count)}
-      end
-    end)
+    tree = trees[y][x]
+
+    (y + 1)..(Enum.count(trees) - 1)
+      |> Enum.reduce([], &([trees[&1][x] | &2]))
+      |> Enum.reverse()
+      |> chunk_trees()
+      |> Enum.reduce_while(0, fn tree_pair, count ->
+        count_next_trees(tree_pair, tree, count)
+      end)
     |> IO.inspect(label: :bottom)
   end
 
   defp visible_from_left({x, y}, trees) do
-    Enum.reduce_while((x - 1)..0, 0, fn next_x, count ->
-      if trees[y][next_x] < trees[y][next_x + 1] do
-        {:cont, count + 1}
-      else
-        {:halt, (if count == 0, do: 1, else: count)}
-      end
-    end)
+    tree = trees[y][x]
+
+    (x - 1)..0
+      |> Enum.reduce([], &([trees[y][&1] | &2]))
+      |> Enum.reverse()
+      |> chunk_trees()
+      |> Enum.reduce_while(0, fn tree_pair, count ->
+        count_next_trees(tree_pair, tree, count)
+      end)
     |> IO.inspect(label: :left)
   end
 
   defp visible_from_right({x, y}, trees) do
-    Enum.reduce_while((x + 1)..(Enum.count(trees[0]) - 1), 0, fn next_x, count ->
-      if trees[y][next_x] < trees[y][next_x - 1] do
-        {:cont, count + 1}
-      else
-        {:halt, (if count == 0, do: 1, else: count)}
-      end
-    end)
+    tree = trees[y][x]
+
+    (x + 1)..(Enum.count(trees[0]) - 1)
+      |> Enum.reduce([], &([trees[y][&1] | &2]))
+      |> Enum.reverse()
+      |> chunk_trees()
+      |> Enum.reduce_while(0, fn tree_pair, count ->
+        count_next_trees(tree_pair, tree, count)
+      end)
     |> IO.inspect(label: :right)
   end
 
