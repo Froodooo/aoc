9d797ad746dffc15dc58660dce7a0f4aaa0745fa
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2022-12-11 10:50:18 +0100

    Solutions for day 11 part 2

--- aoc22/lib/day11/b.ex
+++ aoc22/lib/day11/b.ex
@@ -3,12 +3,22 @@ defmodule AoC22.Day11.B do
   alias AoC22.Day11.Monkey
 
   def solve(input) do
-    input
-    |> Utils.input()
-    |> String.split("\n\n")
-    |> Enum.map(&Monkey.parse/1)
-    |> to_map()
-    |> run()
+    monkeys =
+      input
+      |> Utils.input()
+      |> String.split("\n\n")
+      |> Enum.map(&Monkey.parse/1)
+      |> to_map()
+
+    test_product =
+      monkeys
+      |> Enum.reduce([], fn {_, %Monkey{test: test}}, acc ->
+        [test | acc]
+      end)
+      |> Enum.product()
+
+    monkeys
+    |> run(test_product)
     |> most_active()
     |> Enum.product()
   end
@@ -29,40 +39,47 @@ defmodule AoC22.Day11.B do
     |> Map.new()
   end
 
-  defp run(monkeys) do
+  defp run(monkeys, test_product) do
     Enum.reduce(1..10000, monkeys, fn _, acc ->
-      do_round(acc)
+      do_round(acc, test_product)
     end)
   end
 
-  defp do_round(monkeys) do
-    Enum.reduce(0..(Enum.count(monkeys) - 1), monkeys, &update_in_round/2)
+  defp do_round(monkeys, test_product) do
+    Enum.reduce(0..(Enum.count(monkeys) - 1), monkeys, &update_in_round(&1, &2, test_product))
   end
 
-  defp update_in_round(index, monkeys) do
+  defp update_in_round(index, monkeys, test_product) do
     monkeys =
       update_in(monkeys, [Access.key!(index)], fn monkey ->
         %Monkey{monkey | inspected: monkey.inspected + length(monkey.items)}
       end)
 
-    do_turn(monkeys[index], monkeys)
+    do_turn(monkeys[index], monkeys, test_product)
   end
 
-  defp do_turn(%Monkey{items: items} = monkey, monkeys) do
-    Enum.reduce(items, monkeys, &update_in_turn(&1, &2, monkey))
+  defp do_turn(%Monkey{items: items} = monkey, monkeys, test_product) do
+    Enum.reduce(items, monkeys, &update_in_turn(&1, &2, monkey, test_product))
   end
 
-  defp update_in_turn(item, monkeys, %Monkey{
-         monkey: nr,
-         operation: operation,
-         test: test,
-         test_true: test_true,
-         test_false: test_false
-       }) do
+  defp update_in_turn(
+         item,
+         monkeys,
+         %Monkey{
+           monkey: nr,
+           operation: operation,
+           test: test,
+           test_true: test_true,
+           test_false: test_false
+         },
+         test_product
+       ) do
     new_item =
       item
       |> do_operation(operation)
-      # |> do_bored()
+      |> rem(test_product)
+
+    # |> do_bored()
 
     throw_to =
       new_item
--- aoc22/test/day11/b_test.exs
+++ aoc22/test/day11/b_test.exs
@@ -4,6 +4,6 @@ defmodule AoC22.Day11.BTest do
   alias AoC22.Day11.B
 
   test "solve" do
-    assert B.solve("input/11_test_1.txt") == :result
+    assert B.solve("input/11_test_1.txt") == 2_713_310_158
   end
 end
