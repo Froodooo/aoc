c261862af2a72a3a0c01288102e14709f6a143b0
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2022-12-18 13:10:23 +0100

    Solution for day 18 part 2

--- aoc22/lib/day18/b.ex
+++ aoc22/lib/day18/b.ex
@@ -9,75 +9,58 @@ defmodule AoC22.Day18.B do
       |> Enum.map(&parse_cube/1)
 
     {max_x, max_y, max_z} = max_axes(cubes)
-    all_cubes = init_cube(max_x, max_y, max_z)
+    # all_cubes = init_cube(max_x, max_y, max_z)
 
-    _trapped_sides =
-      all_cubes
-      |> trapped(cubes, {max_x, max_y, max_z})
-      |> Enum.count()
-      |> Kernel.*(6)
-
-    # unconnected_sides(cubes) - trapped_sides
-
-    :result
+    unconnected_sides(cubes, {max_x, max_y, max_z})
   end
 
-  defp trapped(all_cubes, cubes, max_axes) do
-    Enum.filter(all_cubes, fn cube -> interior?([cube], cubes, max_axes) end)
-  end
-
-  defp interior?([], _cubes, _max_axes), do: true
-
-  # Flood-fill (node):
-  # 1. Set Q to the empty queue or stack.
-  # 2. Add node to the end of Q.
-  # 3. While Q is not empty:
-  # 4.   Set n equal to the first element of Q.
-  # 5.   Remove first element from Q.
-  # 6.   If n is Inside:
-  #        Set the n
-  #        Add the node to the west of n to the end of Q.
-  #        Add the node to the east of n to the end of Q.
-  #        Add the node to the north of n to the end of Q.
-  #        Add the node to the south of n to the end of Q.
-  # 7. Continue looping until Q is exhausted.
-  # 8. Return.
-  defp interior?([{x, y, z} | rest], cubes, max_axes) do
-    cond do
-      true ->
-        Enum.reduce(
-          [
-            {x - 1, y, z},
-            {x + 1, y, z},
-            {x, y - 1, z},
-            {x, y + 1, z},
-            {x, y, z - 1},
-            {x, y, z + 1}
-          ],
-          rest,
-          fn cube, queue -> append(queue, cube, cubes, max_axes) end
-        )
-    end
-  end
-
-  defp append(queue, {x, y, z}, cubes, {max_x, max_y, max_z})
-       when x >= 0 and y >= 0 and z >= 0 and x <= max_x and y <= max_y and z <= max_z do
-    case {x, y, z} in cubes do
-      false -> queue ++ [{x, y, z}]
-      true -> queue
-    end
-  end
-
-  defp append(queue, _cube, _cubes, _max_axes), do: queue
-
-  defp init_cube(max_x, max_y, max_z) do
-    Enum.flat_map(0..(max_x - 1), fn x ->
-      Enum.flat_map(0..(max_y - 1), fn y ->
-        Enum.map(0..(max_z - 1), fn z ->
-          {x, y, z}
-        end)
-      end)
+  defp unconnected_sides(cubes, max_axes) do
+    Enum.reduce(cubes, 0, fn {x, y, z}, acc ->
+      count({x + 1, y, z}, cubes, max_axes) +
+        count({x - 1, y, z}, cubes, max_axes) +
+        count({x, y + 1, z}, cubes, max_axes) +
+        count({x, y - 1, z}, cubes, max_axes) +
+        count({x, y, z + 1}, cubes, max_axes) +
+        count({x, y, z - 1}, cubes, max_axes) +
+        acc
     end)
+  end
+
+  defp count(cube, cubes, max_axes) do
+    case interior?([cube], cubes, [], max_axes, false) do
+      true -> 0
+      false -> 1
+    end
+  end
+
+  defp interior?([], _cubes, _seen, _max_axes, seen_outside?), do: !seen_outside?
+
+  defp interior?([{x, y, z} | rest], cubes, seen, {max_x, max_y, max_z}, seen_outside?) do
+    cond do
+      {x, y, z} in cubes ->
+        interior?(rest, cubes, seen, {max_x, max_y, max_z}, seen_outside?)
+
+      {x, y, z} in seen ->
+        interior?(rest, cubes, seen, {max_x, max_y, max_z}, seen_outside?)
+
+      length(seen) > 5000 ->
+        false
+
+      # x <= 0 or x >= max_x or y <= 0 or y >= max_y or z <= 0 or z >= max_z ->
+      #   interior?(rest, cubes, seen, {max_x, max_y, max_z}, true)
+
+      true ->
+        (rest ++
+           [
+             {x - 1, y, z},
+             {x + 1, y, z},
+             {x, y - 1, z},
+             {x, y + 1, z},
+             {x, y, z - 1},
+             {x, y, z + 1}
+           ])
+        |> interior?(cubes, [{x, y, z} | seen], {max_x, max_y, max_z}, seen_outside?)
+    end
   end
 
   def max_axes(cubes) do
