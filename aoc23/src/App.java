3b459ff24fa2ca46e7cee419714bd5e99e9514ba
Yordi Verkroost <y.verkroost@hva.nl>
2023-12-12 16:48:33 +0100

    Code for 2023 day 12 part two that never finishes

--- aoc23/src/App.java
+++ aoc23/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Day12 day12 = new Day12(Utils.readFile("aoc23\\input\\12.txt"));
+        Day12 day12 = new Day12(Utils.readFile("aoc23\\input\\12_sample.txt"));
         System.out.println(day12.partA());
-        // System.out.println(day12.partB());
+        System.out.println(day12.partB());
     }
 }
