46519bddc4d4b682001b1d7ad4f0d7c619770df2
Yordi Verkroost <y.verkroost@hva.nl>
2023-12-18 10:58:03 +0100

    Progress for 2023 day 17

--- aoc23/src/day17/Map.java
+++ aoc23/src/day17/Map.java
@@ -27,6 +27,21 @@ public class Map {
         public int compareTo(Node o) {
             return this.distance - o.distance;
         }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+            Node node = (Node) obj;
+            return x == node.x && y == node.y && distance == node.distance && direction == node.direction
+                    && directionCount == node.directionCount;
+        }
+
+        public String toString() {
+            return y + " " + x + " " + distance + " " + direction + " " + directionCount;
+        }
     }
 
     public Map(String input) {
@@ -41,19 +56,23 @@ public class Map {
     }
 
     public int dijkstra() {
-        int[][] distances = new int[grid.length][grid[0].length];
-        for (int y = 0; y < distances.length; y++) {
-            Arrays.fill(distances[y], Integer.MAX_VALUE);
-        }
-        distances[0][0] = 0;
+        // int[][] distances = new int[grid.length][grid[0].length];
+        // for (int y = 0; y < distances.length; y++) {
+        //     Arrays.fill(distances[y], Integer.MAX_VALUE);
+        // }
+        // distances[0][0] = 0;
 
         Set<Node> visited = new HashSet<Node>();
 
         PriorityQueue<Node> queue = new PriorityQueue<Node>();
-        queue.add(new Node(0, 0, 0, 0, 0));
+        queue.add(new Node(0, 0, 0, -1, -1));
 
         while (!queue.isEmpty()) {
+            // System.out.println("=====");
+            // System.out.println(queue.element());
             Node current = queue.poll();
+            // System.out.println(current.y + " " + current.x + " " + current.distance + " "
+            // + current.direction + " " + current.directionCount);
 
             if (visited.contains(current)) {
                 continue;
@@ -85,15 +104,18 @@ public class Map {
                     continue;
                 }
 
-                // if ((newDirection + 2) % 4 == current.direction) {
-                // continue;
-                // }
+                if ((newDirection + 2) % 4 == current.direction) {
+                    continue;
+                }
 
                 int newDistance = current.distance + grid[newY][newX];
-                if (newDistance < distances[newY][newX]) {
-                    distances[newY][newX] = newDistance;
-                    queue.add(new Node(newX, newY, newDistance, newDirection, newDirectionCount));
-                }
+                // if (newDistance < distances[newY][newX]) {
+                    // distances[newY][newX] = newDistance;
+                    Node newNode = new Node(newX, newY, newDistance, newDirection, newDirectionCount);
+                    queue.add(newNode);
+                    // System.out.println("Added " + newNode);
+                    // System.out.println(queue.element());
+                // }
             }
         }
 
