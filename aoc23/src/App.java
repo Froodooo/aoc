f28011d637bb3656787ca7193a2204854a7495c2
Yordi Verkroost <yordiverkroost@hotmail.com>
2023-12-19 08:15:22 +0100

    Setup for 2023 day 19

--- aoc23/src/App.java
+++ aoc23/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Day18 day18 = new Day18(Utils.readFile("aoc23\\input\\18_sample.txt"));
-        System.out.println(day18.partA());
-        // System.out.println(day18.partB());
+        Day19 day19 = new Day19(Utils.readFile("aoc23\\input\\19_sample.txt"));
+        System.out.println(day19.partA());
+        // System.out.println(day19.partB());
     }
 }
---
+++ aoc23/src/Day19.java
@@ -0,0 +1,16 @@
+public class Day19 {
+    private String input;
+
+    public Day19(String input) {
+        this.input = input;
+
+    }
+
+    public int partA() {
+        return -1;
+    }
+
+    public int partB() {
+        return -1;
+    }
+}
