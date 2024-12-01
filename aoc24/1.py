39c3ddb32d0a2ae0993071bb56516cfda40f6946
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-12-01 09:30:49 +0100

    Cleanup day 1

--- aoc24/1.py
+++ aoc24/1.py
@@ -1,26 +1,31 @@
 from utils import *
 
-input = read_to_list("./1.in")
+def setup(path):
+  input = read_to_list(path)
 
-list_left = []
-list_right = []
+  list_left = []
+  list_right = []
 
-for numbers in input:
-  left, right = numbers.split()
-  list_left.append(int(left))
-  list_right.append(int(right))
+  for numbers in input:
+    left, right = numbers.split()
+    list_left.append(int(left))
+    list_right.append(int(right))
 
-list_left.sort()
-list_right.sort()
+  list_left.sort()
+  list_right.sort()
 
-def part_one():
+  return list_left, list_right
+
+def part_one(path):
+  list_left, list_right = setup(path)
   sum = 0
   for i in range(len(list_left)):
     sum += abs(list_left[i] - list_right[i])
 
-  print(sum)
+  return sum
 
-def part_two():
+def part_two(path):
+  list_left, list_right = setup(path)
   sum = 0
   seen = {}
   for number in list_left:
@@ -28,7 +33,15 @@ def part_two():
       seen[number] = number * list_right.count(number)
     sum += seen[number]
   
-  print(sum)
+  return sum
 
-part_one()
-part_two()
+assert part_one("./1.sample") == 11
+assert part_two("./1.sample") == 31
+
+assert part_one("./1.in") == 2176849
+assert part_two("./1.in") == 23384288
+
+path = "./1.in"
+result_one = part_one(path)
+result_two = part_two(path)
+print(result_one, result_two)
