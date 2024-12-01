5ebdb8597b41eb82f560c022f66b7b3d03708aba
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-12-01 08:56:04 +0100

    Solutions day 1

--- aoc24/1.py
+++ aoc24/1.py
@@ -1,4 +1,31 @@
 from utils import *
 
-content = read_to_list("./sample.txt")
-print(content)
+input = read_to_list("./1.in")
+
+list_left = []
+list_right = []
+
+for numbers in input:
+  left, right = numbers.split()
+  list_left.append(int(left))
+  list_right.append(int(right))
+
+list_left.sort()
+list_right.sort()
+
+def part_one():
+  sum = 0
+  for i in range(len(list_left)):
+    sum += abs(list_left[i] - list_right[i])
+
+  print(sum)
+
+def part_two():
+  sum = 0
+  for number in list_left:
+    sum += number * list_right.count(number)
+  
+  print(sum)
+
+part_one()
+part_two()
