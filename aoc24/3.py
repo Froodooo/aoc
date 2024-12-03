e6b54c39154fd0b7532b1a0010cb109737a08054
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-12-03 06:29:05 +0100

    Solution for day 3 both parts

---
+++ aoc24/3.py
@@ -0,0 +1,49 @@
+from utils import *
+import re
+
+INPUT_FILE = "./3.in"
+SAMPLE_FILE = "./3.sample"
+
+def part_one(path):
+  input = read_to_line(path)
+
+  mul_pattern = re.compile(r'mul\((\d{1,3}),(\d{1,3})\)')
+  matches = re.finditer(mul_pattern, input)
+
+  sum = 0
+  for match in matches:
+    sum += int(match.group(1)) * int(match.group(2))
+
+  return sum
+
+def part_two(path):
+  input = read_to_line(path)
+
+  mul_pattern = re.compile(r'(mul\((\d{1,3}),(\d{1,3})\))|(don\'t\(\))|(do\(\))')
+  matches = re.finditer(mul_pattern, input)
+
+  enabled = True
+  sum = 0
+  for match in matches:
+    if match.group(4):
+      enabled = False
+      continue
+    if match.group(5):
+      enabled = True
+      continue
+    
+    if enabled:
+      sum += int(match.group(2)) * int(match.group(3))
+
+  return sum
+
+assert part_one(SAMPLE_FILE) == 161
+assert part_two(SAMPLE_FILE) == 48
+
+assert part_one(INPUT_FILE) == 187825547
+assert part_two(INPUT_FILE) == 85508223
+
+path = INPUT_FILE
+result_one = part_one(path)
+result_two = part_two(path)
+print(result_one, result_two)
--- aoc24/utils.py
+++ aoc24/utils.py
@@ -2,3 +2,8 @@ def read_to_list(path):
     with open(path) as f:
         lines = f.read().splitlines()
         return lines
+
+def read_to_line(path):
+    with open(path) as f:
+        line = f.read()
+        return line
