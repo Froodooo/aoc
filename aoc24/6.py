b0f9e772387840d3c2e580c7fa8d2dd9ae72169b
Yordi Verkroost <y.verkroost@hva.nl>
2024-12-06 09:03:13 +0100

    Refactor day 6 part 1

--- aoc24/6.py
+++ aoc24/6.py
@@ -17,26 +17,10 @@ turn = {
   'R': 'D'
 }
 
-obstacle = {
-  'U': 'L',
-  'D': 'R',
-  'L': 'D',
-  'R': 'U'
-}
-
-def get_start(map):
-  for i in range(len(map)):
-      for j in range(len(map[i])):
-          if map[i][j] == '^':
-              return (i, j)
-
-def part_one(path):
-  map = [list(x) for x  in read_to_list(path)]
-  (pos_y, pos_x) = get_start(map)
-  (dir_x, dir_y) = directions['U']
-  direction = 'U'
+def get_path(map, pos_x, pos_y, direction):
   path = set()
   path.add((pos_x, pos_y))
+  (dir_x, dir_y) = directions['U']
 
   while pos_x + dir_x >= 0 and pos_x + dir_x < len(map) and pos_y + dir_y >= 0 and pos_y + dir_y < len(map[pos_x]):
     while map[pos_y + dir_y][pos_x + dir_x] == "#":
@@ -45,25 +29,25 @@ def part_one(path):
     pos_x += dir_x
     pos_y += dir_y
     path.add((pos_x, pos_y))
+  return path
 
-  print(len(path))
+def get_start(map):
+  for i in range(len(map)):
+    for j in range(len(map[i])):
+       if map[i][j] == '^':
+         return (i, j)
 
-map = [list(x) for x  in read_to_list(SAMPLE_FILE)]
-(pos_y, pos_x) = get_start(map)
-(dir_x, dir_y) = directions['U']
-direction = 'U'
-path = set()
-obstructions = set()
-path.add((pos_x, pos_y, direction))
+def part_one(path):
+  map = [list(x) for x  in read_to_list(path)]
+  (pos_y, pos_x) = get_start(map)
+  direction = 'U'
+  path = get_path(map, pos_x, pos_y, direction)
+  return len(path)
 
-while pos_x + dir_x >= 0 and pos_x + dir_x < len(map) and pos_y + dir_y >= 0 and pos_y + dir_y < len(map[pos_x]):
-  if any(x == pos_x and y == pos_y for (x, y, direction) in path):
-     print("Found obstruction at: ", pos_x, pos_y)
-  while map[pos_y + dir_y][pos_x + dir_x] == "#":
-    direction = turn[direction]
-    (dir_x, dir_y) = directions[direction]
-  pos_x += dir_x
-  pos_y += dir_y
-  path.add((pos_x, pos_y, direction))
+assert part_one(SAMPLE_FILE) == 41
 
-print(len(path))
+assert part_one(INPUT_FILE) == 4977
+
+path = SAMPLE_FILE
+result_one = part_one(path)
+print(result_one)
