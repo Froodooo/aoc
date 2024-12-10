d0769d258fac481ae734994b2ead09a87f5e61cf
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-12-10 19:43:16 +0100

    Add tests for day 10

--- aoc24/10.py
+++ aoc24/10.py
@@ -5,8 +5,6 @@ SAMPLE_FILE = "./10.sample"
 
 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-map = [[int(y) for y in list(x)] for x in read_to_list(INPUT_FILE)]
-
 nine_heights = set()
 total = 0
 
@@ -25,13 +23,32 @@ def get_trails(x, y, prev, map):
           if map[yy][xx] == prev + 1:
               get_trails(xx, yy, map[y][x], map)
 
-total_trails = 0
-for y in range(len(map)):
-    for x in range(len(map[y])):
-        if map[y][x] == 0:
-            get_trails(x, y, 0, map)
-            total_trails += len(nine_heights)
-            nine_heights.clear()
+def solve(path, p1=True):
+    global nine_heights
+    global total
 
-print(total_trails) #p1
-print(total) #p2
+    nine_heights.clear()
+    total = 0
+
+    map = [[int(y) for y in list(x)] for x in read_to_list(path)]
+    total_trails = 0
+    for y in range(len(map)):
+        for x in range(len(map[y])):
+            if map[y][x] == 0:
+                get_trails(x, y, 0, map)
+                total_trails += len(nine_heights)
+                nine_heights.clear()
+    
+    return total_trails if p1 else total
+
+
+assert solve(SAMPLE_FILE) == 36
+assert solve(SAMPLE_FILE, False) == 81
+
+assert solve(INPUT_FILE) == 496
+assert solve(INPUT_FILE, False) == 1120
+
+path = INPUT_FILE
+result_one = solve(path)
+result_two = solve(path, False)
+print(result_one, result_two)
