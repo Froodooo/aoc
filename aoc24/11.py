3022e7d54241306f29ba57a10107713f1e1f38c6
Froodooo <yordiverkroost@hotmail.com>
2024-12-11 14:38:36 +0100

    Solution for day 11 part 2

--- aoc24/11.py
+++ aoc24/11.py
@@ -1,29 +1,56 @@
 from utils import *
-from collections import deque
 
 INPUT_FILE = "./11.in"
 SAMPLE_FILE = "./11.sample"
 
 def blink(stones):
-    for _ in range(len(stones)):
-        stone = stones.popleft()
+    stones_copy = dict(stones)
+    current_stones = list(stones.keys())
+    for stone in current_stones:
+        stone_count = stones_copy[stone]
+        stones[stone] -= stones_copy[stone]
+            
         if stone == 0:
-            stones.append(1)
+            if stones.get(1) is None:
+                stones[1] = 0
+            stones[1] += stone_count
         elif len(str(stone)) % 2 == 0:
             digits = len(str(stone))
             half = 10 ** (digits // 2)
             left = stone // half
             right = stone % half
-            stones.append(left)
-            stones.append(right)
+            if stones.get(left) is None:
+                stones[left] = 0
+            stones[left] += stone_count
+            if stones.get(right) is None:
+                stones[right] = 0
+            stones[right] += stone_count
         else:
-            stones.append(stone * 2024)
+            if stones.get(stone * 2024) is None:
+                stones[stone * 2024] = 0
+            stones[stone * 2024] += stone_count
+    return stones
 
-stones = deque(int(x) for x in read_to_line(SAMPLE_FILE).split())
-print(stones)
+def part_one(path):
+    stones = {int(x): 1 for x in read_to_line(path).split()}
+    for i in range(25):
+        stones = blink(stones)
+    return sum(stones.values())
 
-for i in range(75):
-    print(i + 1)
-    blink(stones)
+def part_two(path):
+    stones = {int(x): 1 for x in read_to_line(path).split()}
+    for i in range(75):
+        stones = blink(stones)
+    return sum(stones.values())
 
-print(len(stones))
+
+assert part_one(SAMPLE_FILE) == 55312
+# assert part_two(SAMPLE_FILE) == 81
+
+assert part_one(INPUT_FILE) == 203609
+assert part_two(INPUT_FILE) == 240954878211138
+
+path = INPUT_FILE
+result_one = part_one(path)
+result_two = part_two(path)
+print(result_one, result_two)
