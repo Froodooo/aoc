d1ba309864f31ea1f979bc0f75966a7b96f28b66
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-12-11 22:21:21 +0100

    Cleanup day 11

--- aoc24/11.py
+++ aoc24/11.py
@@ -3,49 +3,43 @@ from utils import *
 INPUT_FILE = "./11.in"
 SAMPLE_FILE = "./11.sample"
 
+def split_stone(stone):
+    digits = len(str(stone))
+    half = 10 ** (digits // 2)
+    left = stone // half
+    right = stone % half
+    return left, right
+
 def blink(stones):
     stones_copy = dict(stones)
-    current_stones = list(stones.keys())
-    for stone in current_stones:
-        stone_count = stones_copy[stone]
-        stones[stone] -= stones_copy[stone]
-            
+    for stone in stones_copy.keys():
+        stone_value = stones_copy[stone]
+        stones[stone] -= stone_value   
         if stone == 0:
-            if stones.get(1) is None:
-                stones[1] = 0
-            stones[1] += stone_count
+            stones[1] = stones.get(1, 0) + stone_value
         elif len(str(stone)) % 2 == 0:
-            digits = len(str(stone))
-            half = 10 ** (digits // 2)
-            left = stone // half
-            right = stone % half
-            if stones.get(left) is None:
-                stones[left] = 0
-            stones[left] += stone_count
-            if stones.get(right) is None:
-                stones[right] = 0
-            stones[right] += stone_count
+            left, right = split_stone(stone)
+            stones[left] = stones.get(left, 0) + stone_value
+            stones[right] = stones.get(right, 0) + stone_value
         else:
-            if stones.get(stone * 2024) is None:
-                stones[stone * 2024] = 0
-            stones[stone * 2024] += stone_count
+            stones[stone * 2024] = stones.get(stone * 2024, 0) + stone_value          
     return stones
 
-def part_one(path):
+def solve(path, iterations):
     stones = {int(x): 1 for x in read_to_line(path).split()}
-    for i in range(25):
+    for i in range(iterations):
         stones = blink(stones)
     return sum(stones.values())
 
+def part_one(path):
+    return solve(path, 25)
+
 def part_two(path):
-    stones = {int(x): 1 for x in read_to_line(path).split()}
-    for i in range(75):
-        stones = blink(stones)
-    return sum(stones.values())
+    return solve(path, 75)
 
 
 assert part_one(SAMPLE_FILE) == 55312
-# assert part_two(SAMPLE_FILE) == 81
+# assert part_two(SAMPLE_FILE) == 240954878211138
 
 assert part_one(INPUT_FILE) == 203609
 assert part_two(INPUT_FILE) == 240954878211138
