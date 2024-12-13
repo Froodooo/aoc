5f2ee4f23dbecbd1cbd302039fa264467f8fdaf6
Yordi Verkroost <y.verkroost@hva.nl>
2024-12-13 15:52:48 +0100

    Solution for day 12 both parts

--- aoc24/12.py
+++ aoc24/12.py
@@ -4,11 +4,12 @@ INPUT_FILE = "./12.in"
 SAMPLE_FILE = "./12.sample"
 
 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-garden = [list(x) for x in read_to_list(INPUT_FILE)]
 seen = set()
 area = set()
+sides = {}
+sides_count = 0
 
-def find_area(x, y):
+def find_area(x, y, plot, garden):
     global seen
 
     if x < 0 or y < 0 or x >= len(garden[0]) or y >= len(garden): return
@@ -17,7 +18,7 @@ def find_area(x, y):
     area.add((x, y))
     seen.add((x, y))
     for dx, dy in directions:
-        find_area(x + dx, y + dy)
+        find_area(x + dx, y + dy, plot, garden)
   
 def find_perimeter(area):
     perimeter = 0
@@ -27,14 +28,55 @@ def find_perimeter(area):
                 perimeter += 1
     return perimeter
 
-price = 0
-for y in range(len(garden)):
-    for x in range(len(garden[y])):
-        if (x, y) in seen: continue
-        area = set()
-        plot = garden[y][x]
-        find_area(x, y)
-        perimeter = find_perimeter(area)
-        price += len(area) * perimeter
-        # print(garden[y][x], len(area), perimeter, area)
-print(price)
+def find_sides(area):
+    global sides_count
+
+    for x, y in area:
+        for dx, dy in directions:
+            if (x + dx, y + dy) not in area:
+                if (dx in [-1, 1]):
+                    sides[('x', x, dx)] = sides.get(('x', x, dx), []) + [y]
+                if (dy in [-1, 1]):
+                    sides[('y', y, dy)] = sides.get(('y', y, dy), []) + [x]
+    for values in sides.values():
+        values.sort()
+        sides_count += 1
+        for i in range(1, len(values)):
+            if values[i] - values[i - 1] > 1:
+                sides_count += 1
+        
+
+def solve(path):
+  global area
+  global sides
+  global sides_count
+  global seen
+
+  garden = [list(x) for x in read_to_list(path)]
+
+  seen = set()
+  price = 0
+  price_discount = 0
+  for y in range(len(garden)):
+      for x in range(len(garden[y])):
+          if (x, y) in seen: continue
+          area = set()
+          sides = {}
+          sides_count = 0
+          plot = garden[y][x]
+          find_area(x, y, plot, garden)
+          perimeter = find_perimeter(area)
+          find_sides(area)
+          price += len(area) * perimeter
+          price_discount += len(area) * sides_count
+  return price, price_discount
+
+price, price_discount = solve(SAMPLE_FILE)
+assert price == 1930
+assert price_discount == 1206
+
+price, price_discount = solve(INPUT_FILE)
+assert price == 1465112
+assert price_discount == 893790
+
+print(price, price_discount)
