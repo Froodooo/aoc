f920243fac17ebd738f7a5b9bcb7bdaf78c6d179
Froodooo <yordiverkroost@hotmail.com>
2024-12-16 15:51:18 +0100

    Solution for day 16 part 1

--- aoc24/16.py
+++ aoc24/16.py
@@ -1,51 +1,51 @@
 from utils import *
-from collections import deque
+import heapq
 
 INPUT_FILE = "./16.in"
 SAMPLE_FILE = "./16.sample"
 
-def bfs(start, goal, walls):
-    sx, sy = start
-    queue = deque([([[(sx, sy, (1, 0))], 0])])
-    # print(queue.popleft())
-    scores = {start: 0}
-    results = []
-    while queue:
-        path, score = queue.popleft()
-        # print(path)
-        # print(path, score)
-        # pdx, pdy = queue_dir.popleft()
-        x, y, dir = path[-1]
-        if (x, y) == goal:
-            results.append((path, score))
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
-            direction_score = 0 if (dx, dy) == dir else 1000
-            if (nx, ny) not in walls and (nx, ny, dir) not in path:
-                # print(scores)
-                if (nx, ny) in scores:
-                    new_score = scores[(x, y)] + direction_score + 1
-                    if new_score > scores[(nx, ny)]:
-                        continue
-                queue.append((path + [(nx, ny, (dx, dy))], score + direction_score + 1))
-                scores[(nx, ny)] = scores[(x, y)] + direction_score + 1
-    print([x[1] for x in results])
-    path = min(results, key=lambda x: x[1])
-    for loc in path[0]:
-        x, y, _ = loc
-        # print(f"({x}, {y}), {score[loc]}")
-    return path[1]
-
 directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+directions_inverted = {
+    (1, 0): (-1, 0),
+    (0, 1): (0, -1),
+    (-1, 0): (1, 0),
+    (0, -1): (0, 1)
+}
 
-maze = [list(x) for x in read_to_list(INPUT_FILE)]
+def bfs(start, end, walls):
+    sx, sy = start
+    queue = [(0, sx, sy, 1, 0)]
+    visited = {(sx, sy, 1, 0)}
+    while queue:
+        cost, x, y, cdx, cdy = heapq.heappop(queue)
+        visited.add((x, y, cdx, cdy))
+        if (x, y) == end:
+            return cost
+        for dx, dy in directions:
+            if directions_inverted[(dx, dy)] == (cdx, cdy):
+                continue
+            ndx = cdx if (dx, dy) == (cdx, cdy) else dx
+            ndy = cdy if (dx, dy) == (cdx, cdy) else dy
+            nx = x + dx if (cdx, cdy) == (dx, dy) else x
+            ny = y + dy if (cdx, cdy) == (dx, dy) else y
+            if (nx, ny) in walls:
+                continue
+            if (nx, ny, ndx, ndy) in visited:
+                continue
+            cost_add = 1 if (cdx, cdy) == (dx, dy) else 1000
+            heapq.heappush(queue, (cost + cost_add, nx, ny, ndx, ndy))
 
-walls = {(x, y) for y in range(len(maze)) for x in range(len(maze[y])) if maze[y][x] == "#"}
-start = [(x, y) for y in range(len(maze)) for x in range(len(maze[y])) if maze[y][x] == "S"][0]
-end = [(x, y) for y in range(len(maze)) for x in range(len(maze[y])) if maze[y][x] == "E"][0]
+def part_one(path):
+    maze = [list(x) for x in read_to_list(path)]
 
-result = bfs(start, end, walls)
-print(result)
+    walls = {(x, y) for y in range(len(maze)) for x in range(len(maze[y])) if maze[y][x] == "#"}
+    start = [(x, y) for y in range(len(maze)) for x in range(len(maze[y])) if maze[y][x] == "S"][0]
+    end = [(x, y) for y in range(len(maze)) for x in range(len(maze[y])) if maze[y][x] == "E"][0]
 
-# 83480 too low
-# 85480 correct
+    return bfs(start, end, walls)
+
+assert part_one(SAMPLE_FILE) == 11048
+assert part_one(INPUT_FILE) == 85480
+
+part_one = part_one(INPUT_FILE)
+print(part_one)
