7728a10509efeebd08175f96669d1efe3abe66fc
Yordi Verkroost <yordiverkroost@hotmail.com>
2024-12-18 10:31:02 +0100

    Solution for day 18 part 1

---
+++ aoc24/18.py
@@ -0,0 +1,36 @@
+from utils import *
+from collections import deque
+
+INPUT_FILE = "./18.in"
+SAMPLE_FILE = "./18.sample"
+
+def bfs(start, goal, width, height, corrupted):
+    queue = deque([[start]])
+    seen = set([start])
+    while queue:
+        path = queue.popleft()
+        x, y = path[-1]
+        if (x, y) == goal:
+            return path
+        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+            if 0 <= x2 <= width and 0 <= y2 <= height and (x2, y2) not in corrupted and (x2, y2) not in seen:
+                queue.append(path + [(x2, y2)])
+                seen.add((x2, y2))
+
+def part_one(path, max_x = 70, max_y = 70, sample_size = 1024):
+    start = (0, 0)
+    end = (max_x, max_y)
+
+    bytes = [tuple(int(y) for y in x.split(",")) for x in read_to_list(path)]
+    bytes_sample = bytes[:sample_size]
+
+    memory = set(bytes_sample)
+    path = bfs(start, end, max_x, max_y, memory)
+    return len(path) - 1
+
+assert part_one(SAMPLE_FILE, 6, 6, 12) == 22
+assert part_one(INPUT_FILE) == 246
+
+part_one = part_one(INPUT_FILE)
+print(part_one)
+
