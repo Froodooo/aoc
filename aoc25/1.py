d8384944efa5e1153888c93b3c04abb1609a0c5e
Yordi Verkroost <yordiverkroost@hotmail.com>
2025-12-01 06:29:49 +0100

    chore: small cleanup

--- aoc25/1.py
+++ aoc25/1.py
@@ -6,13 +6,16 @@ from utils import read_to_list
 
 def zero_passes(pointer:int, direction:str, clicks:int) -> int:
     count = 0
-    
+
     for _ in range(clicks):
         pointer = (pointer + 1) % 100 if direction == 'R' else (pointer - 1) % 100
-        if pointer == 0:
-            count += 1
+        count += 1 if pointer == 0 else 0
 
     return count
+
+
+def zero_point(pointer:int) -> int:
+    return 1 if pointer == 0 else 0
 
 
 def rotate(pointer:int, direction:str, clicks:int) -> int:
@@ -32,8 +35,7 @@ def part_one(data: List[str]) -> int:
         direction = line[0]
         clicks = int(line[1:])
         pointer = rotate(pointer, direction, clicks)
-        if pointer == 0:
-            zero_pointer += 1
+        zero_pointer += zero_point(pointer)
 
     return zero_pointer
 
