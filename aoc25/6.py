fff98d1bce6b8c8796c602dfc16eaaff3edca658
Yordi Verkroost <yordiverkroost@hotmail.com>
2025-12-06 09:38:50 +0100

    chore: add some empty lines for readability

--- aoc25/6.py
+++ aoc25/6.py
@@ -12,22 +12,27 @@ def parse_input(path: Path) -> List[str]:
 def part_one(data: List[str]) -> int:
     lines = [item.split() for item in data]
     problems = [[int(item) for item in line] for line in lines[:-1]]
+
     operations = lines[-1]
     columns = problems[0]
+
     for line in problems[1:]:
         for i in range(len(columns)):
             if operations[i] == '+':
                 columns[i] += line[i]
             elif operations[i] == '*':
                 columns[i] *= line[i]
+
     return sum(columns)
 
 
 def part_two(data: List[str]) -> int:
     problems = data[:-1]
     operations = data[-1].split()
+
     total = 0
     numbers = []
+
     for i in range(len(problems[0]) - 1, -2, -1):
         if all(problem[i] == ' ' for problem in problems) or i == -1:
             operation = operations.pop()
@@ -39,4 +44,5 @@ def part_two(data: List[str]) -> int:
         else:
             column = ''.join(problem[i] for problem in problems).strip()
             numbers.append(int(column))
+
     return total
