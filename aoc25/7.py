30d9cb3322afdb0413ffc73ace8396620025119e
Yordi Verkroost <yordiverkroost@hotmail.com>
2025-12-07 21:01:43 +0100

    chore: refactor code for day 7

--- aoc25/7.py
+++ aoc25/7.py
@@ -1,36 +1,33 @@
 from pathlib import Path
 from typing import List
+from functools import lru_cache
 
 from utils import read_to_list
 
-
 SEEN_SPLITS = set()
-SEEN = set()
 
-def beam(manifold: List[List[str]], start: tuple[int, int], path: str = ""):
+
+def parse_input(path: Path) -> List[List[str]]:
+    return [list(line) for line in read_to_list(str(path))]
+
+
+def beam(manifold: List[List[str]], start: tuple[int, int]) -> int:
     (sr, sc) = start
 
-    if sr >= len(manifold):
-        return
+    @lru_cache(maxsize=None)
+    def dp(sr: int, sc: int) -> int:
+        if sr >= len(manifold):
+            return 1
+        
+        if manifold[sr][sc] == '^':
+            if (sr, sc) not in SEEN_SPLITS:
+                SEEN_SPLITS.add((sr, sc))
+
+            return dp(sr, sc - 1) + dp(sr, sc + 1)
+        else:
+            return dp(sr + 1, sc)
     
-    if (sr, sc) not in SEEN:
-        SEEN.add((sr, sc))
-
-    if manifold[sr][sc] == '^':
-        if (sr, sc) not in SEEN_SPLITS:
-            SEEN_SPLITS.add((sr, sc))
-        split_left = (sr, sc - 1)
-        split_right = (sr, sc + 1)
-
-        if split_left not in SEEN and split_right not in SEEN:
-            beam(manifold, split_left, path + f"({sr},{sc - 1})")
-            beam(manifold, split_right, path + f"({sr},{sc + 1})")
-        if split_left not in SEEN and split_right in SEEN:
-            beam(manifold, split_left, path + f"({sr},{sc - 1})")
-        if split_right not in SEEN and split_left in SEEN:
-            beam(manifold, split_right, path + f"({sr},{sc + 1})")
-    else:
-        beam(manifold, (sr + 1, sc), path)
+    return dp(sr, sc)
 
 
 def find_start(manifold: List[List[str]]) -> tuple[int, int]:
@@ -38,20 +35,16 @@ def find_start(manifold: List[List[str]]) -> tuple[int, int]:
         for c, val in enumerate(row):
             if val == 'S':
                 return r, c
-    raise ValueError("Start position 'S' not found")
-
-
-def parse_input(path: Path) -> List[List[str]]:
-    return [list(line) for line in read_to_list(str(path))]
 
 
 def part_one(data: List[str]) -> int:
     (sr, sc) = find_start(data)
     beam(data, (sr, sc))
+
     return len(SEEN_SPLITS)
 
 
 def part_two(data: List[str]) -> int:
     (sr, sc) = find_start(data)
-    beam(data, (sr, sc))
-    return 0
+
+    return beam(data, (sr, sc))
