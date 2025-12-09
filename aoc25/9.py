29546fb013d1d61dafd1da3d4b0bb664f41a8d69
Yordi Verkroost <y.verkroost@hva.nl>
2025-12-09 13:12:43 +0100

    chore: refator logic into functions

--- aoc25/9.py
+++ aoc25/9.py
@@ -5,8 +5,18 @@ from shapely.geometry import Polygon
 from utils import read_to_list
 
 
-def parse_input(path: Path) -> List[str]:
+def parse_input(path: Path) -> List[tuple]:
     return [tuple(int(x) for x in line.split(',')) for line in read_to_list(str(path))]
+
+
+def area_size(x1: int, y1: int, x2: int, y2: int) -> int:
+    return (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+
+
+def poly_rectangle_area(x1: int, y1: int, x2: int, y2: int) -> Polygon:
+    x_min, x_max = min(x1, x2), max(x1, x2)
+    y_min, y_max = min(y1, y2), max(y1, y2)
+    return Polygon([(x_min, y_min), (x_min, y_max), (x_max, y_max), (x_max, y_min)])
 
 
 def part_one(data: List[str]) -> int:
@@ -15,7 +25,7 @@ def part_one(data: List[str]) -> int:
         for j in range(i + 1, len(data)):
             x1, y1 = data[i]
             x2, y2 = data[j]
-            area = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+            area = area_size(x1, y1, x2, y2)
             if area > largest:
                 largest = area
     return largest
@@ -28,11 +38,9 @@ def part_two(data: List[str]) -> int:
         for j in range(i + 1, len(data)):
             x1, y1 = data[i]
             x2, y2 = data[j]
-            x_min, x_max = min(x1, x2), max(x1, x2)
-            y_min, y_max = min(y1, y2), max(y1, y2)
-            rectangle = Polygon([(x_min, y_min), (x_min, y_max), (x_max, y_max), (x_max, y_min)])
+            rectangle = poly_rectangle_area(x1, y1, x2, y2)
             if rectangle.within(poly_area):
-                area = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+                area = area_size(x1, y1, x2, y2)
                 if area > largest:
                     largest = area
     return largest
