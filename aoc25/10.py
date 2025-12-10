039df6f06b291a5fc589e5ac99665ba9da509a18
Froodooo <yordiverkroost@hotmail.com>
2025-12-10 12:31:52 +0100

    chore: format code

--- aoc25/10.py
+++ aoc25/10.py
@@ -11,22 +11,23 @@ def parse_line_diagram(line: str) -> str:
     return sub('[#]', '1', sub('[.]', '0', sub('[\\[\\]]', '', line)))
 
 
-def parse_button_schematics(line: str, length: int) -> List[int]:
+def parse_button_wirings(line: str, length: int) -> List[int]:
     return [[int(y) for y in x.split(',')] for x in [sub('[()]', '', c) for c in line[1:len(line)-1]]]
 
 
 def parse_joltage_requirements(line: str) -> tuple:
     return tuple([int(x) for x in sub('[\\{\\}]', '', line[-1]).split(',')])
 
+
 def parse_input(path: Path) -> List[str]:
     data = [line.split(" ") for line in read_to_list(str(path))]
     lines = []
     for line in data:
         light_diagram = parse_line_diagram(line[0])
-        button_schematics = parse_button_schematics(line, len(light_diagram))
+        button_wirings = parse_button_wirings(line, len(light_diagram))
         joltage_requirements = parse_joltage_requirements(line)
-        lines.append((light_diagram, button_schematics, joltage_requirements))
-        
+        lines.append((light_diagram, button_wirings, joltage_requirements))
+
     return lines
 
 
@@ -34,17 +35,17 @@ def bitmask_to_int(b):
     return int(b, 2)
 
 
-def find_shortest_xor_combo(bits, target):
+def find_shortest_xor_combo(button_wirings, target):
     target_int = bitmask_to_int(target)
-    values = [bitmask_to_int(b) for b in bits]
+    values = [bitmask_to_int(b) for b in button_wirings]
 
-    for r in range(1, len(bits) + 1):
-        for combo in combinations(range(len(bits)), r):
+    for r in range(1, len(button_wirings) + 1):
+        for combo in combinations(range(len(button_wirings)), r):
             xor_value = 0
             for i in combo:
                 xor_value ^= values[i]
             if xor_value == target_int:
-                return [bits[i] for i in combo]
+                return [button_wirings[i] for i in combo]
 
 
 def button_wiring_to_bitmask(button_wirings_list: List[int], length: int) -> str:
@@ -55,7 +56,7 @@ def button_wiring_to_bitmask(button_wirings_list: List[int], length: int) -> str
             bit = '1' if i in button_wiring else '0'
             button_bitmap += bit
         button_wirings.append(button_bitmap)
-        
+
     return button_wirings
 
 
@@ -66,16 +67,16 @@ def button_wiring_to_vector(button_wirings: List[int], length: int) -> List[int]
         for bit in button:
             bitmask[bit] = 1
         button_vectors.append(bitmask)
-        
+
     return button_vectors
 
 
 def configure_machine(button_vectors: List[tuple], joltage_requirements: List[int], start: tuple):
     length = len(joltage_requirements)
-    
+
     queue = deque([(start, 0)])
     visited = {start}
-    
+
     while queue:
         state, dist = queue.popleft()
 
@@ -90,24 +91,27 @@ def configure_machine(button_vectors: List[tuple], joltage_requirements: List[in
 
             if new_state not in visited:
                 visited.add(new_state)
-                queue.append((new_state, dist + 1)) 
+                queue.append((new_state, dist + 1))
 
 
 def part_one(data: List[str]) -> int:
     button_presses = 0
-    for (light_diagram, button_schematics, _) in data:
-        button_schematics = button_wiring_to_bitmask(button_schematics, len(light_diagram))
-        result = find_shortest_xor_combo(button_schematics, light_diagram)
+    for (light_diagram, button_wirings, _) in data:
+        button_wirings = button_wiring_to_bitmask(
+            button_wirings, len(light_diagram))
+        result = find_shortest_xor_combo(button_wirings, light_diagram)
         button_presses += len(result)
 
-    return button_presses 
+    return button_presses
 
 
 def part_two(data: List[str]) -> int:
     button_presses = 0
-    for (_, button_schematics, joltage_requirements) in data:
-        button_vectors = button_wiring_to_vector(button_schematics, len(joltage_requirements))
+    for (_, button_wirings, joltage_requirements) in data:
+        button_vectors = button_wiring_to_vector(
+            button_wirings, len(joltage_requirements))
         joltage_start = tuple([0] * len(joltage_requirements))
-        button_presses += configure_machine(button_vectors, joltage_requirements, joltage_start)
-        
+        button_presses += configure_machine(button_vectors,
+                                            joltage_requirements, joltage_start)
+
     return button_presses
